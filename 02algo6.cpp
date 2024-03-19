4d5ae501050cb0f840df1cf62c1853c23a913089
ansunho <ansunho123@naver.com>
2024-03-19 23:53:58 +0900

    02algo basic1

--- 02algo6.cpp
+++ 02algo6.cpp
@@ -40,3 +40,34 @@ int main()
         cout << arr[j] << ' ';
     }
 }
+
+/*
+void reverse(int a, int b){
+  for(int i = 0; i < (b - a + 1) / 2; i++)
+    swap(num[a+i], num[b-i]);
+}
+
+
+
+//STL reverse를 이용한 풀이
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  for(int i = 1; i <= 20; i++) num[i] = i;
+
+  for(int i = 1; i <= 10; i++) {
+    int a, b;
+    cin >> a >> b;
+    reverse(num+a, num+b+1);
+  }
+
+  for(int i = 1; i <= 20; i++) cout << num[i] << ' ';
+}
+
+reverse 함수
+reverse(v.begin(),v.begin()+4) 1~4번쨰
+
+
+*/
