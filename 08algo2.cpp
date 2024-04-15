2fb4a3bc5c034a8a4098d5dc5a8f062beb419dc2
ansunho <ansunho123@naver.com>
2024-04-15 19:31:30 +0900

    08algo stack boj

---
+++ 08algo2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+
+    cin >> n;
+    int cnt = 0;
+    while (n--)
+    {
+        string str;
+        stack<char> s;
+
+        cin >> str;
+        for (auto c : str)
+        {
+
+            if (!s.empty() && s.top() == c)
+                s.pop();
+            else
+                s.push(c);
+        }
+
+        if (s.empty())
+        {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+}
