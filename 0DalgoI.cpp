84e7bf273e594eb8965a20c57c36bff896745ca3
ansunho <ansunho123@naver.com>
2024-11-18 22:58:20 +0900

    0Dalgo simulation boj

--- 0DalgoI.cpp
+++ 0DalgoI.cpp
@@ -1,7 +1,13 @@
-// Authored by : SciEm
-// Co-authored by : BaaaaaaaaaaarkingDog
-// http://boj.kr/74da3b10100849d68340661e3e748159
-#include <bits/stdc++.h>
+// boj 15684
+/*
+1) i번째 사다리가 i로 갈 수 있는지 확인 하는 함수
+2) 백트래킹 구현 3중 for문
+123 124 125 126 134 135 136 ....
+*/
+
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 #define X first
@@ -12,20 +18,27 @@ int idxs[3];
 vector<pair<int, int>> coords; // 고를 수 있는 가로선만을 저장할 벡터
 int n, m, h;
 
-// i번째가 i인지 사다리를 타며 확인
+// i번째 사다리가 i로 갈 수 있는지 확인 하는 함수
 bool check()
 {
     for (int j = 1; j <= n; j++)
     {
-        int cur = j;
+        int idx = j;
         for (int i = 1; i <= h; i++)
-        {
-            if (ladder[i][cur - 1])
-                cur--;
-            else if (ladder[i][cur])
-                cur++;
+        { // 사다리 1번이면 옆으로 못감
+            if (idx - 1 != 0)
+            {
+                if (ladder[i][idx - 1])
+                    idx--;
+            }
+            // 사다리 오른쪽이면 옆으로 못감
+            else if (idx + 1 <= n)
+                if (ladder[i][idx])
+                {
+                    idx++;
+                }
         }
-        if (cur != j)
+        if (idx != j)
             return false;
     }
     return true;
@@ -37,6 +50,7 @@ int main()
     cin.tie(0);
 
     cin >> n >> m >> h;
+
     while (m--)
     {
         int a, b;
@@ -61,6 +75,7 @@ int main()
 
     int ans = 0x7f7f7f7f;
     int sz = coords.size();
+
     for (int i = 0; i < sz; i++)
     {
         ladder[coords[i].X][coords[i].Y] = true;
