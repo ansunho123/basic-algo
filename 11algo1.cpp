2d8ac443b6aa83c247ac8e58e71a6c5dccf7a7e6
ansunho <ansunho123@naver.com>
2024-12-19 20:12:42 +0900

    11algo Greedy boj

--- 11algo1.cpp
+++ 11algo1.cpp
@@ -1,25 +1,31 @@
-// boj 10844
+// boj 11047 동전 0
+// dp말고 그리디 풀이.
+// 가장 금액이 큰 동전부터 나누기 시작하면 참.
 
 #include <iostream>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
-int n, k;
-int a[15];
+int a[11];
 
-int main(void)
+int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    int ans = 0;
+    int n, k;
+
     cin >> n >> k;
+
     for (int i = 0; i < n; i++)
+    {
         cin >> a[i];
+    }
+    int ans = 0;
+
     for (int i = n - 1; i >= 0; i--)
     {
         ans += k / a[i];
         k %= a[i];
     }
+
     cout << ans;
 }
