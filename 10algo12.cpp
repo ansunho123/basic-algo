e31f7473c0235ab715514840db977e139f08074a
ansunho <ansunho123@naver.com>
2024-12-19 19:18:41 +0900

    10algo DP boj

--- 10algo12.cpp
+++ 10algo12.cpp
@@ -1,34 +1,52 @@
 // boj 10844
+// d[i][j] : i의 길이를 가지고 뒤에 숫자가 j인 계단수의 갯수
+// d[i][j] = d[i-1][j-1] + d[i-1][j+1]; 이다.
+// 단 j==0이면 d[i-1][1]에서만 추가 되고 j==9이면 d[i-1][8]에서만 받는다.
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
+using namespace std;
 
 long long d[101][10];
+
 int main()
 {
-    ios::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
+
     int n;
     cin >> n;
-    for (int i = 1; i <= 9; ++i)
-        d[1][i] = 1;
 
-    for (int i = 2; i <= n; ++i)
+    for (int i = 1; i <= 9; i++)
     {
-        for (int k = 0; k <= 9; ++k)
-        {
-            if (k != 0)
-                d[i][k] += d[i - 1][k - 1];
-            if (k != 9)
-                d[i][k] += d[i - 1][k + 1];
-            d[i][k] %= 1000000000;
+        d[1][i] = 1;
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 0; j <= 9; j++)
+        { // j==0이면 d[i-1][1]에서만 추가 되고 j==9이면 d[i-1][8]에서만 받는다.
+            if (j != 0)
+            {
+                d[i][j] += d[i - 1][j - 1];
+            }
+            if (j != 9)
+            {
+                d[i][j] += d[i - 1][j + 1];
+            }
+
+            d[i][j] %= 1000000000;
         }
     }
+
     long long ans = 0;
-    for (int i = 0; i <= 9; ++i)
+
+    for (int i = 0; i <= 9; i++)
     {
         ans += d[n][i];
     }
     ans %= 1000000000;
+
     cout << ans;
 }
