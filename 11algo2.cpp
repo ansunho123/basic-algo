0ff744e73ac71f136e8c76aa2ec2fb848a0a3b94
ansunho <ansunho123@naver.com>
2024-12-19 22:39:56 +0900

    11algo Greedy boj

--- 11algo2.cpp
+++ 11algo2.cpp
@@ -1,29 +1,39 @@
-
-
+// 1931 boj 그리디
+// 대표적인 그리디 문제.
+// 끝나는 시간으로 정렬 후 (같으면 시작시간 빠른 순)
+// 끝나는 시간과 현재 시간에 맞게 계속 비교.
+// 끝나는 시간 작은 순으로 정렬이 되었으므로 그리디 가능.
 #include <iostream>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
 int n;
-pair<int, int> s[100005]; // schedule, 정렬의 편의를 위해 {끝 시간, 시작 시간}으로 저장
+pair<int, int> s[100005]; // <끝나는 시간, 시작시간> 정렬 때문에.
 
-int main(void)
+int main()
 {
-    ios::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
+
     cin >> n;
-    for (int i = 0; i < n; i++)
-        cin >> s[i].second >> s[i].first;
-    sort(s, s + n); // 먼저 끝나는 시간을 비교하고, 끝나는 시간이 동일하면 시작 시간 순으로 정렬
-    int ans = 0;
-    int t = 0; // 현재 시간
+    int cnt = 0;
+
     for (int i = 0; i < n; i++)
     {
-        if (t > s[i].second)
-            continue;   // 시작 시간이 현재 시간보다 이전인 회의라면 무시
-        ans++;          // 시작 시간이 현재 시간 이후인 회의를 찾았으므로 회의의 수에 1 증가
-        t = s[i].first; // 현재 시간을 s[i]의 끝나는 시간으로 변경한다.
+        cin >> s[i].second >> s[i].first;
     }
-    cout << ans;
+    sort(s, s + n);
+
+    int t = 0; // 현재시간
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i].second < t)
+        { // 시작 시간보다 현재시간이 크면 못함.
+            continue;
+        }
+        t = s[i].first; // 끝나는 시간으로
+        cnt++;
+    }
+    cout << cnt;
 }
