97ad913cb4168e86f0b9e43cc562d2afcda201b5
ansunho <ansunho123@naver.com>
2024-12-20 16:32:54 +0900

    11algo Greedy boj

--- 11algo3.cpp
+++ 11algo3.cpp
@@ -1,6 +1,11 @@
+// 1931 boj 그리디
+// 대표적인 그리디 문제.
+// 끝나는 시간으로 정렬 후 (같으면 시작시간 빠른 순)
+// 끝나는 시간과 현재 시간에 맞게 계속 비교.
+// 끝나는 시간 작은 순으로 정렬이 되었으므로 그리디 가능.
 #include <iostream>
 #include <algorithm>
-
+#include <vector>
 using namespace std;
 
 int n;
