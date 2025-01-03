486f65884c557f2f3497c0a6e41b8936b702bf09
ansunho <ansunho123@naver.com>
2025-01-03 14:57:34 +0900

    11algo Greedy boj

--- 11algo4.cpp
+++ 11algo4.cpp
@@ -1,33 +1,40 @@
+// boj 2217
+//  n개 중에서 k개의 로프를 선택해서 어떤 것이 중량을 가장 크게 할 수 있는지.
+//  O(2^N) 브루트 포스 시간초과
+// 그리디
+// 1) n-i = k개의 로프를 선택하는게 가장 큰 값인지
+// 2) 로프를 k 선택했을 때 가장 큰 로프 k개가 가장 큰 값.
+// O(N);
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
-
 using namespace std;
 
-int tmp, ans;
-int sign = 1;
-int main(void)
+int a[100002];
+
+int main()
 {
-    ios::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    string input;
-    cin >> input;
-    for (char c : input)
+    int n;
+    int ans = 0;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
     {
-        if (c == '+' || c == '-')
-        {
-            ans += tmp * sign;
-            if (c == '-')
-                sign = -1;
-            tmp = 0;
-        }
-        else
-        {
-            tmp *= 10;
-            tmp += c - '0';
-        }
+        cin >> a[i];
     }
-    ans += tmp * sign;
+
+    sort(a, a + n);
+    for (int i = 0; i < n; i++)
+    {
+        ans = max(ans, a[i] * (n - i));
+        // 1) n-i = k개의 로프를 선택하는게 가장 큰 값인지
+        // 2) 로프를 k 선택했을 때 가장 큰 로프 k개가 가장 큰 값.
+    }
+
     cout << ans;
 }
